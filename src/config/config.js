cc62f62af9b0b30e5c17b007b599f744080d6ef0
Rene Bridgman <renebridgman@Renes-iMac.local>
2023-02-08 10:45:34 +0100

    start creating signup page

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARW4S_s0LQAjP3V2KUbzdWZNukpxhULII",
+    authDomain: "social-media-project-6c66c.firebaseapp.com",
+    projectId: "social-media-project-6c66c",
+    storageBucket: "social-media-project-6c66c.appspot.com",
+    messagingSenderId: "490660258362",
+    appId: "1:490660258362:web:885c2e14f4534466148b03"
+  };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,8 +1,58 @@
+// hooks
+import { useState } from 'react'
+
+// styles
 import './Signup.css'
 
 
 export default function Signup() {
+
+  // set initial state for user input
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  // handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+  }
+
   return (
-    <div>Signup</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Username:</span>
+          <input 
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Email:</span>
+          <input 
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Password:</span>
+          <input 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+
+        <button>Signup</button>
+      </form>
+    </div>
   )
 }
