2a2dc14a9a49d64427306becd31f13d473bcb28f
Rene Bridgman <renebridgman@Renes-iMac.local>
2023-02-08 10:28:08 +0100

    initial commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,25 @@
+import './Home.css'
+
+
+export default function Home() {
+  return (
+    <div className="main-wrapper">
+
+      <div className="welcome-container">
+        <h1>Welcome Back, René</h1>
+      </div>
+
+      <div className="main-content-container">
+        <div className="browse-users-container">
+
+        </div>
+        <div className="news-feed-container">
+
+        </div>
+        <div className="friends-list-container">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
